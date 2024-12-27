0c409a62b9fe746a5425149091c2ccf4f3027b00
Anil Pal <anilcodes01@gmail.com>
2024-12-27 11:37:59 +0530

    added middleware

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { authOptions } from "./utils/authOptions";
 export default async function App() {
   const session = await getServerSession(authOptions);
 
-  // Redirect to /info if the user is not logged in
+ 
   if (!session) {
     return <script>{`window.location.href = '/info'`}</script>;
   }
 
-  // Render home page content if logged in
+  
   return (
     <div className="min-h-screen flex items-center justify-center text-black">
       This is home page
